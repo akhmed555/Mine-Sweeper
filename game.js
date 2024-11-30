cf3ca299a252ecc9fdb3bc9e0025fb6aa1e7a1d2
akhmed555 <71272915+akhmed555@users.noreply.github.com>
2024-11-30 20:59:07 +0300

    Add files via upload

--- game.js
+++ game.js
@@ -90,6 +90,8 @@
 
       updateDigits(container, number) {
          if (!container) return;
+		 
+		 if(number>999)number=999;
 
          const classesToRemove = ['digit-0', 'digit-1', 'digit-2', 'digit-3', 'digit-4', 'digit-5', 'digit-6', 'digit-7', 'digit-8', 'digit-9'];
          const digits = String(number).padStart(3, "0").split("");
